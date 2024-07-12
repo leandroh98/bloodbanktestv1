bf89580499e7e113cd46435439bda0c5743b48ec
Leandro <lh2015052384@virtual.upt.pe>
2024-07-12 16:56:54 -0500

    version1

---
+++ Bank.Domain.Tests/Features/CuentaPersonalPruebas.cs
@@ -0,0 +1,88 @@
+using Bank.Domain;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+namespace Bank.Domain.Tests.Features
+{
+    [Binding]
+    public sealed class CuentaPersonalPruebas
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private CuentaAhorro _cuenta { get; set; }
+        private string _error { get; set; }
+        private bool _es_error { get; set; } = false;
+        
+        public CuentaPersonalPruebas(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("la nueva cuenta numero (.*)")]
+        public void DadoUnaNuevaCuenta(string numeroCuenta)
+        {
+            try
+            {
+                var cliente = Cliente.Registrar("Juan Perez");
+                _cuenta = CuentaAhorro.Aperturar(numeroCuenta, cliente, 1);
+            }
+            catch (System.Exception ex)
+            {
+                _es_error = true; 
+                _error = ex.Message;
+            }            
+        }
+
+        // [Given("con saldo (.*)")]
+        // public void YConSaldo(decimal monto)
+        // {
+        //     CuandoYoDeposito(monto);
+        // }
+
+        [Given("con saldo (.*)")]
+        [When("deposito (.*)")]
+        public void CuandoYoDeposito(decimal monto)
+        {
+            try
+            {
+                _cuenta.Depositar(monto);
+            }
+            catch (System.Exception ex)
+            {
+                _es_error = true; 
+                _error = ex.Message;
+            }
+        }
+
+        [When("retiro (.*)")]
+        public void CuandoYoRetiro(decimal monto)
+        {
+            try
+            {
+                _cuenta.Retirar(monto);
+                //_resultado = _cuenta.Saldo;
+            }
+            catch (System.Exception ex)
+            {
+                _es_error = true; 
+                _error = ex.Message;
+            }
+        }
+
+        [Then("el saldo nuevo deberia ser (.*)")]
+        public void EntoncesElResultadoDeberiaSer(decimal resultado)
+        {
+            Assert.AreEqual(_cuenta.Saldo, resultado);
+        }        
+
+        [Then("deberia ser error")]
+        public void EntoncesDeberiaMostrarseError()
+        {
+            Assert.IsTrue(_es_error);
+        }
+
+        [Then("deberia mostrarse el error: (.*)")]
+        public void EntoncesDeberiaMostrarseError(string error)
+        {
+            Assert.AreEqual(_error, error);
+        }
+    }
+}
---
+++ Bank.Domain/CuentaPersonal.cs
@@ -0,0 +1,39 @@
+﻿namespace Bank.Domain
+{
+    public class CuentaPersonal
+    {
+        public const string ERROR_MONTO_MENOR_IGUAL_A_CERO = "El monto no puede ser menor o igual a 0";
+        public int IdCuenta { get; private set; }
+        public string NumeroCuenta { get; private set; }
+        public virtual Paciente Propietario { get; private set; }
+        public int IdPropietario { get; private set; }
+        public decimal Tasa { get; private set; }
+        public decimal Saldo { get; private set; }
+        public DateTime FechaApertura { get; private set; }
+        public bool Estado { get; private set; }
+        
+        public static CuentaPersonal Aperturar(string _numeroCuenta, Paciente _propietario, decimal _tasa)
+        {
+            return new CuentaAhorro()
+            {
+                NumeroCuenta = _numeroCuenta,
+                Propietario = _propietario,
+                IdPropietario = _propietario.IdCliente,
+                Tasa = _tasa,
+                Saldo = 0
+            };
+        }     
+        public void Depositar(decimal monto)
+        {
+            if (monto <= 0)
+                throw new Exception (ERROR_MONTO_MENOR_IGUAL_A_CERO);
+            Saldo += monto;
+        }
+        public void Retirar(decimal monto)
+        {
+            if (monto <= 0)
+                throw new Exception (ERROR_MONTO_MENOR_IGUAL_A_CERO);
+            Saldo -= monto;
+        }
+    }
+}
